8e9aec2842d99fbab2fb2c57ce41f28c1eba5780
Gangulaprathapreddy <108295633+Gangulaprathapreddy@users.noreply.github.com>
2024-02-28 11:11:24 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -10,33 +10,15 @@ data "aws_ami" "amazon-linux" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["amazon"]
 }
 
 resource "aws_instance" "dev_machine" {
-  ami           = data.aws_ami.amazon-linux.id
+  ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.exam_testing.key_name  # Use the key_name attribute of the key pair resource
+  key_name = "terraform"
 
-  provisioner "local-exec" {
-    command     = "ansible-playbook -i ${aws_instance.dev_machine.public_ip}  nginx.yaml"
-    working_dir = path.module  # Added to set the working directory
+  tags = {
+    Environment = "dev"
+    Name = "${var.name}-server"
   }
-}
-
-resource "aws_key_pair" "exam_testing" {
-  key_name = "aws-exam-testing"
-}
-
-output "ip" {
-  value = aws_instance.dev_machine.public_ip
-}
-
-output "publicName" {
-  value = aws_instance.dev_machine.public_dns
-}
-
-output "public_key" {
-  value = aws_key_pair.exam_testing.public_key
 }
